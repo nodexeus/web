c715ca515b43e306a24a9cd811189c1f1f43d230
Sebastijan Dumančić <sebastijan@prototyp.digital>
2022-08-23 15:40:39 +0200

    Ported global style variables and spacing

--- styles/global.styles.ts
+++ styles/global.styles.ts
@@ -7,47 +7,135 @@ import { breakpoints } from './variables.styles';
 
 export const globalStyles = css`
   :root {
-    /** Colors */
-    --color-black: #000000;
-    --color-white: #ffffff;
-    --color-primary: #4f2784;
-    --color-progressBar: #d3d9d9;
-    --color-secondary: #e94187;
-    --color-tertiary: #f77f00;
-    --color-text: #2e2e38;
-    --color-textNegative: #fcfcfd;
-    --color-textLight: #9d9daf;
-    --color-textGray: #c7c7d1;
-    --color-textGrayLight: #73738c;
-    --color-succes: #259a51;
-    --color-error: #c32424;
-    --color-noted: #e9ad39;
-    --color-background: #eaeaea;
-    --color-backgroundLight: #f5f5f5;
-    --color-backgroundDark: #16191d;
-    --color-backgroundGray: #383b43;
-    --color-backgroundGrayLight: #73738c;
-    --color-border: #c7c7d1;
-    --color-borderLight: #d4d4d4;
-    --color-borderDark: #727272;
-    --color-backgroundHero: #d3d9d9;
+    /* --- PRIMARY --- */
+    --color-primary: hsl(90, 84%, 75%);
+    --color-primary-o0: hsla(90, 84%, 75%, 0);
+    --color-primary-o30: hsla(90, 84%, 75%, 0.3);
 
-    /** Font family */
-    --font-family-primary: 'Neutra2-Book, serif';
-    --font-family-primaryLight: 'Neutra2-Light, serif';
+    /* --- SECONDARY --- */
+    --color-secondary: hsl(29, 82%, 80%);
+    --color-secondary-o0: hsla(29, 82%, 80%, 0);
+    --color-secondary-o30: hsla(29, 82%, 80%, 0.3);
 
-    /** Font weight */
-    --fontWeight-regular: 400;
-    --fontWeight-medium: 500;
-    --fontWeight-semiBold: 600;
-    --fontWeight-bold: 700;
-    --fontWeight-extraBold: 800;
+    /* --- TERTIARY --- */
+    --color-tertiary: hsl(60, 5%, 85%);
 
-    /** Border radius */
-    --radius-small: '6px';
-    --radius-medium: '16px';
-    --radius-rounded: '1000px';
+    /* --- TEXT --- */
+    --color-text-1: hsl(160, 4%, 14%);
+    --color-text-1-o20: hsla(160, 4%, 14%, 0.2);
+    --color-text-1-o30: hsla(160, 4%, 14%, 0.3);
+    --color-text-1-o70: hsla(160, 4%, 14%, 0.7);
+    --color-text-2: hsl(90, 2%, 37%);
+    --color-text-3: hsl(90, 3%, 65%);
+    --color-text-4: hsl(90, 5%, 81%);
+    --color-text-5: hsl(90, 29%, 97%);
+    --color-text-5-o3: hsla(90, 29%, 97%, 0.03);
+    --color-text-5-o10: hsla(90, 29%, 97%, 0.1);
+    --color-text-5-o20: hsla(90, 29%, 97%, 0.2);
+    --color-text-5-o30: hsla(90, 29%, 97%, 0.3);
+
+    /* --- UTILITY --- */
+    --color-utility-success: hsl(143, 61%, 37%);
+    --color-utility-success-o10: hsla(143, 61%, 37%, 0.1);
+    --color-utility-warning: hsl(0, 79%, 69%);
+    --color-utility-warning-o10: hsla(0, 69%, 45%, 0.1);
+    --color-utility-note: hsl(40, 80%, 57%);
+
+    /* --- BACKGROUND --- */
+    --color-overlay-background-1: hsl(160, 3%, 22%);
+    --color-overlay-background-2: hsl(180, 4%, 15%);
+    --color-overlay-background-2-o50: hsla(180, 4%, 15%, 0.5);
+
+    /* --- BORDER --- */
+    --color-border-1: hsl(160, 4%, 14%);
+    --color-border-2: hsl(90, 2%, 37%);
+    --color-border-3: hsl(0, 0%, 73%);
+    --color-border-4: hsl(0, 0%, 83%);
+    --color-border-5: hsl(90, 29%, 97%);
+
+    /* --- INPUT --- */
+    --color-input-background: hsl(165, 4%, 20%);
+
+    /* --- OTHERS --- */
+    --color-foreground-secondary: hsl(160, 4%, 14%); /* Black */
+    --color-foreground-secondary-o10: hsla(160, 4%, 14%, 0.1); /* Black */
+    --color-foreground-tertiary: hsl(160, 5%, 21%); /* Black 2 */
+    --color-shadow-o10: hsla(0, 0%, 0%, 0.1);
+    --color-shadow-o80: hsla(0, 0%, 0%, 0.8);
   }
+
+  /* --- LIGHT THEME --- */
+  :root.theme--light {
+    /* TODO: Add light theme colors, these are just for testing */
+    --color-overlay-background-1: hsl(240, 20%, 85%);
+
+    --color-text-3: hsl(160, 4%, 14%);
+    --color-text-1: hsl(96, 3%, 65%);
+  }
+
+  /* --- FONT SIZES --- */
+  --root-font-size: 16;
+  --font-size-button: calc((16 / var(--root-font-size)) * 1rem);
+  --font-size-button-small: calc((13 / var(--root-font-size)) * 1rem);
+  --font-size-microlabel: calc((10 / var(--root-font-size)) * 1rem);
+  --font-size-micro: calc((10 / var(--root-font-size)) * 1rem);
+  --font-size-tiny: calc((12 / var(--root-font-size)) * 1rem);
+  --font-size-label: calc((12 / var(--root-font-size)) * 1rem);
+  --font-size-smaller: calc((13 / var(--root-font-size)) * 1rem);
+  --font-size-small: calc((14 / var(--root-font-size)) * 1rem);
+  --font-size-base: calc((16 / var(--root-font-size)) * 1rem);
+  --font-size-medium: calc((20 / var(--root-font-size)) * 1rem);
+  --font-size-large: calc((24 / var(--root-font-size)) * 1rem);
+  --font-size-xlarge: calc((32 / var(--root-font-size)) * 1rem);
+  --font-size-xxlarge: calc((40 / var(--root-font-size)) * 1rem);
+  --font-size-xxxlarge: calc((48 / var(--root-font-size)) * 1rem);
+  --font-size-huge: calc((60 / var(--root-font-size)) * 1rem);
+
+  /* --- LINE HEIGHTS--- */
+  --line-height-micro: calc((12 / var(--root-font-size)) * 1rem);
+  --line-height-tiny: calc((16 / var(--root-font-size)) * 1rem);
+  --line-height-microlabel: calc((16 / var(--root-font-size)) * 1rem);
+  --line-height-label: calc((16 / var(--root-font-size)) * 1rem);
+  --line-height-smaller: calc((20 / var(--root-font-size)) * 1rem);
+  --line-height-small: calc((20 / var(--root-font-size)) * 1rem);
+  --line-height-base: calc((24 / var(--root-font-size)) * 1rem);
+  --line-height-button-small: calc((20 / var(--root-font-size)) * 1rem);
+  --line-height-button: calc((24 / var(--root-font-size)) * 1rem);
+  --line-height-medium: calc((28 / var(--root-font-size)) * 1rem);
+  --line-height-large: calc((32 / var(--root-font-size)) * 1rem);
+  --line-height-xlarge: calc((40 / var(--root-font-size)) * 1rem);
+  --line-height-xxlarge: calc((44 / var(--root-font-size)) * 1rem);
+  --line-height-xxxlarge: calc((52 / var(--root-font-size)) * 1rem);
+  --line-height-huge: calc((64 / var(--root-font-size)) * 1rem);
+
+  /* --- LETTER SPACINGS --- */
+  --letter-spacing-huge: calc((-2 / var(--root-font-size)) * 1rem);
+  --letter-spacing-xxxlarge: calc((-2 / var(--root-font-size)) * 1rem);
+  --letter-spacing-xxlarge: calc((-1 / var(--root-font-size)) * 1rem);
+  --letter-spacing-button: calc((-0.5 / var(--root-font-size)) * 1rem);
+  --letter-spacing-default: calc((0 / var(--root-font-size)) * 1rem);
+  --letter-spacing-microlabel: calc((1 / var(--root-font-size)) * 1rem);
+  --letter-spacing-label: calc((1.25 / var(--root-font-size)) * 1rem);
+
+  /* --- FONT FAMILIES --- */
+  --font-family-primary: 'Styrene A LC', -apple-system, BlinkMacSystemFont,
+    avenir next, avenir, segoe ui, helvetica neue, helvetica, Ubuntu, roboto,
+    noto, arial, sans-serif;
+
+  /* --- FONT WEIGHTS --- */
+  --font-weight-normal: 400;
+  --font-weight-bold: 700;
+
+  /** --- LEVELS --- */
+  --level-n1: -1;
+  --level-1: 1;
+  --level-2: 2;
+  --level-3: 3;
+  --level-4: 4;
+  --level-9: 9;
+
+  /* --- TRANSITION EASING --- */
+  --transition-easing-cubic: cubic-bezier(0.55, 0.06, 0.68, 0.19);
 
   @font-face {
     font-family: 'Neutra2-Light';
@@ -65,6 +153,24 @@ export const globalStyles = css`
     font-family: 'Neutra2-Demi';
     font-weight: 600;
     src: url('/fonts/Neutra2-Demi.otf') format('opentype');
+  }
+
+  @keyframes rotateClockwise {
+    from {
+      transform: rotate3d(0, 0, 1, 0deg) translate3d(0, 0, 0);
+    }
+    to {
+      transform: rotate3d(0, 0, 1, 360deg) translate3d(0, 0, 0);
+    }
+  }
+
+  @keyframes blink {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
   }
 
   html {
---
+++ styles/utils.spacing.styles.ts
@@ -0,0 +1,120 @@
+import { css } from '@emotion/react';
+
+export const display = {
+  bottom: {
+    micro: css`
+      margin-bottom: 4px;
+    `,
+    small: css`
+      margin-bottom: 8px;
+    `,
+    mediumSmall: css`
+      margin-bottom: 12px;
+    `,
+    medium: css`
+      margin-bottom: 20px;
+    `,
+    large: css`
+      margin-bottom: 40px;
+    `,
+    xLarge: css`
+      margin-bottom: 60px;
+    `,
+    xxLarge: css`
+      margin-bottom: 80px;
+    `,
+    xxxLarge: css`
+      margin-bottom: 120px;
+    `,
+    huge: css`
+      margin-bottom: 160px;
+    `,
+  },
+  top: {
+    micro: css`
+      margin-top: 4px;
+    `,
+    small: css`
+      margin-top: 8px;
+    `,
+    mediumSmall: css`
+      margin-top: 12px;
+    `,
+    medium: css`
+      margin-top: 20px;
+    `,
+    large: css`
+      margin-top: 40px;
+    `,
+    xLarge: css`
+      margin-top: 60px;
+    `,
+    xxLarge: css`
+      margin-top: 80px;
+    `,
+    xxxLarge: css`
+      margin-top: 120px;
+    `,
+    huge: css`
+      margin-top: 160px;
+    `,
+  },
+  left: {
+    micro: css`
+      margin-left: 4px;
+    `,
+    small: css`
+      margin-left: 8px;
+    `,
+    mediumSmall: css`
+      margin-left: 12px;
+    `,
+    medium: css`
+      margin-left: 20px;
+    `,
+    large: css`
+      margin-left: 40px;
+    `,
+    xLarge: css`
+      margin-left: 60px;
+    `,
+    xxLarge: css`
+      margin-left: 80px;
+    `,
+    xxxLarge: css`
+      margin-left: 120px;
+    `,
+    huge: css`
+      margin-left: 160px;
+    `,
+  },
+  right: {
+    micro: css`
+      margin-right: 4px;
+    `,
+    small: css`
+      margin-right: 8px;
+    `,
+    mediumSmall: css`
+      margin-right: 12px;
+    `,
+    medium: css`
+      margin-right: 20px;
+    `,
+    large: css`
+      margin-right: 40px;
+    `,
+    xLarge: css`
+      margin-right: 60px;
+    `,
+    xxLarge: css`
+      margin-right: 80px;
+    `,
+    xxxLarge: css`
+      margin-right: 120px;
+    `,
+    huge: css`
+      margin-right: 160px;
+    `,
+  },
+};
