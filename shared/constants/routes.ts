b24566c31347e3474bc9a3ccf284993f53930b38
Bojan Bušljeta <bojan@prototyp.digital>
2022-08-23 15:11:07 +0200

    add routes file

---
+++ shared/constants/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  Dashboard: '/app/dashboard',
+  HOSTS: '/app/hosts',
+  HOST_GROUP: (id: string) => `/app/hosts/group/${id}`,
+  NODES: /app/denos,
+  NODE_GROUP: (id: string) => `/app/nodes/group/${id}`,
+  LOGIN: '/',
+  REGISTER: '/register',
+};
