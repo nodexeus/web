52faecf7ee9f195c0f68e4a61192a8958417d63a
Bojan Bušljeta <bojan@prototyp.digital>
2022-08-25 15:12:00 +0200

    add layout animation

--- shared/components/Layout/Layout.tsx
+++ shared/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
-import type { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import anime from 'animejs';
 import { layout, layoutWrapper, layoutTitle } from './Layout.styles';
 import { Logo } from '../Logo';
 import { typo } from 'styles/utils.typography.styles';
@@ -9,9 +10,21 @@ type Props = {
 };
 
 export function Layout({ children, title }: Props) {
+  const animateEntry = () =>
+    anime({
+      targets: `#js-auth-layout`,
+      opacity: [0, 1],
+      translateY: [8, 0],
+      easing: 'easeInOutQuad',
+      duration: 400,
+    });
+  useEffect(() => {
+    animateEntry();
+  }, []);
+
   return (
     <main tabIndex={0} id="content" css={[layout]}>
-      <section css={[layoutWrapper, typo.small]}>
+      <section css={[layoutWrapper, typo.small]} id="js-auth-layout">
         <header>
           <Logo />
           <h1 css={[layoutTitle]}>{title}</h1>
