f6970aa9f3f8581cb28f0c1560c76c99d3b7c74c
Sebastijan Dumančić <sebastijan@prototyp.digital>
2022-08-29 14:58:46 +0200

    Migrated SectionDescription

---
+++ shared/components/SectionDescription/SectionDescription.styles.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+import { breakpoints } from 'styles/variables.styles';
+
+export const styles = {
+  base: css`
+    margin-top: 60px;
+  `,
+  text: css`
+    padding-left: 20px;
+    border-left: 1px solid var(--color-primary);
+  `,
+};
