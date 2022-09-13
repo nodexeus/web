d8a28da441be48a807e553d6e671b905f2402f08
Bojan Bušljeta <bojan@prototyp.digital>
2022-09-13 16:43:20 +0200

    redirect to dashboard if already signed in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
-import { LoginFooter, LoginForm } from '@modules/auth';
+import { authAtoms, LoginFooter, LoginForm } from '@modules/auth';
 import { AuthLayout } from '@modules/layout';
 import type { NextPage } from 'next';
+import Router from 'next/router';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 
 const Login: NextPage = () => {
+  const [auth] = useRecoilState(authAtoms.user);
+
+  useEffect(() => {
+    if (auth) {
+      Router.push('/dashboard');
+    }
+  }, []);
   return (
     <AuthLayout layoutTitle="Login">
       <LoginForm />
