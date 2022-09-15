81fc068ade4a902c0a97fce9848cae906d60bf65
Joe <frontendjoe@outlook.com>
2022-09-15 08:55:18 +0100

    switching back to stub client

--- modules/client/apiClient.ts
+++ modules/client/apiClient.ts
@@ -1,5 +1,5 @@
 // import { GrpcClient } from 'blockjoy-mock-grpc/dist/stub_client';
-// import { GrpcClient } from './stub_client';
-import { GrpcClient } from './grpc_client';
+import { GrpcClient } from './stub_client';
+// import { GrpcClient } from './grpc_client';
 
 export const apiClient = new GrpcClient('http://157.245.21.140:80');
