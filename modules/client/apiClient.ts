2dcd8a36a24d390d45e45a966b1d2321ca689d65
Sebastijan Dumančić <sebastijan@prototyp.digital>
2022-09-15 14:14:05 +0200

    Trying out new env based client declaration

--- modules/client/apiClient.ts
+++ modules/client/apiClient.ts
@@ -1,5 +1,8 @@
 // import { GrpcClient } from 'blockjoy-mock-grpc/dist/stub_client';
-import { GrpcClient } from './stub_client';
-// import { GrpcClient } from './grpc_client';
+import { GrpcClient as StubClient } from './stub_client';
+import { GrpcClient as RealClient } from './grpc_client';
 
-export const apiClient = new GrpcClient('http://157.245.21.140:80');
+export const apiClient =
+  process.env.NEXT_PUBLIC_VERCEL_CLIENT_TYPE === 'stub'
+    ? new StubClient('http://157.245.21.140:80')
+    : new RealClient('http://157.245.21.140:80');
