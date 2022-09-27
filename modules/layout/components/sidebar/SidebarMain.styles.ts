f2b41af077deb0051f8b4d8adede43c8f3ce486a
Bojan Bušljeta <bojan@prototyp.digital>
2022-09-27 16:42:58 +0200

    fix styling

--- modules/layout/components/sidebar/SidebarMain.styles.ts
+++ modules/layout/components/sidebar/SidebarMain.styles.ts
@@ -3,7 +3,9 @@ import { ITheme } from 'types/theme';
 
 export const styles = {
   wrapper: css`
-    flex: 1 1 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     padding: 20px 16px;
   `,
   header: (theme: ITheme) => css`
