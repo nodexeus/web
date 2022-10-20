184c6c5ce7ae8bb7036d780aa141a61759da1383
Bojan Bušljeta <bojan@prototyp.digital>
2022-10-20 15:11:20 +0200

    remove old profile

--- modules/layout/components/AppLayout.tsx
+++ modules/layout/components/AppLayout.tsx
@@ -1,7 +1,6 @@
 import Sidebar from './sidebar/Sidebar';
 import Overlay from './overlay/Overlay';
 import { Topbar } from './topbar/Topbar';
-import Profile from './profile/Profile';
 import Page from './page/Page';
 import Breadcrumb from './breadcrumb/Breadcrumb';
 import { NodeAdd } from '@modules/node/components/nodeAdd/NodeAdd';
@@ -20,7 +19,6 @@ export const AppLayout: React.FC<LayoutType> = ({ children, breadcrumb }) => {
         <Sidebar />
         <Overlay />
         <Topbar />
-        <Profile />
         <NodeAdd />
         <OrganizationAdd />
         <EditOrganization />
--- modules/layout/components/profile/Profile.styles.ts
+++
@@ -1,7 +0,0 @@
-import { css } from '@emotion/react';
-
-export const styles = {
-  action: css`
-    width: 100%;
-  `,
-};
