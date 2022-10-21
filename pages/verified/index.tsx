298f4088634d18e51a8a0489be691b761f0c2082
frontend-joe <frontendjoe@outlook.com>
2022-10-22 00:01:21 +0100

    adding verified loader

--- pages/verified/index.tsx
+++ pages/verified/index.tsx
@@ -4,12 +4,43 @@ import { colors } from 'styles/utils.colors.styles';
 import { typo } from 'styles/utils.typography.styles';
 import { spacing } from 'styles/utils.spacing.styles';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { apiClient } from '@modules/client';
+import { css, keyframes } from '@emotion/react';
+import { ITheme } from 'types/theme';
+
+const loaderKeyframes = keyframes`
+  0% { 
+    translate: -100% 0;
+  }
+  100% {
+    translate: 0 0;
+  }
+`;
+
+const styles = {
+  loaderRail: css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 5px;
+    background: rgba(0, 0, 0, 0.3);
+  `,
+  loaderBar: (theme: ITheme) => css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: ${theme.colorPrimary};
+    animation: ${loaderKeyframes} 25s;
+  `,
+};
 
 const Verified: NextPage = () => {
+  const [serverError, setServerError] = useState<string>('');
   const { query } = useRouter();
-
   useEffect(() => {
     (async () => {
       const { token } = query;
@@ -17,6 +48,10 @@ const Verified: NextPage = () => {
       const response: any = await apiClient.registration_confirmation(
         token?.toString()!,
       );
+
+      if (response.code === 20) {
+        setServerError('Error verifying your account, please contact support.');
+      }
 
       console.log('verified', response);
 
@@ -26,6 +61,9 @@ const Verified: NextPage = () => {
 
   return (
     <Layout title="Email being verified.">
+      <div css={styles.loaderRail}>
+        <div css={styles.loaderBar}></div>
+      </div>
       <p css={[typo.small, colors.text3, spacing.bottom.medium]}>
         You will be redirected to the dashboard once complete.
       </p>
