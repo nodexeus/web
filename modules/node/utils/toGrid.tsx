fb2fd78781ddadb4845467d5125d71a9fbe96e99
tstaetter <thomas.staetter@gmail.com>
2022-12-20 01:08:24 +0100

    fix: tmp fix for debugging

--- modules/node/utils/toGrid.tsx
+++ modules/node/utils/toGrid.tsx
@@ -8,6 +8,8 @@ export const toGrid = (
   onCellClick: (args0: any) => void,
 ) => {
   return nodeList?.map((node: any) => {
+    // TODO: delete the replace ASAP!!!
+    const node_type = node.type.replace("}{", "},{");
     return {
       key: node.id,
       component: (
@@ -18,7 +20,7 @@ export const toGrid = (
           cellIcon={<BlockchainIcon blockchainId={node.blockchainId} />}
           cellStatus={<NodeStatus hasBorder status={node.status} />}
           cellType={
-            nodeTypeList.find((n) => n.id === JSON.parse(node.type).id)?.name
+            nodeTypeList.find((n) => n.id === JSON.parse(node_type).id)?.name
           }
         />
       ),
