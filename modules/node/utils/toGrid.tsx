f9bb5105e42ad777166903b1d48a4151c6ef0b0c
frontend-joe <frontendjoe@outlook.com>
2022-12-20 11:04:29 +0000

    node launcher file upload

--- modules/node/utils/toGrid.tsx
+++ modules/node/utils/toGrid.tsx
@@ -9,7 +9,21 @@ export const toGrid = (
 ) => {
   return nodeList?.map((node: any) => {
     // TODO: delete the replace ASAP!!!
-    const node_type = node.type.replace("}{", "},{");
+    // const node_type = node.type.replace('}{', '},{');
+
+    let error = '';
+
+    try {
+      JSON.parse(node.type);
+    } catch (e) {
+      console.log('JSON', node.type.toString());
+      console.log('e', e);
+    }
+
+    const parsedJSON = JSON.parse(node.type);
+
+    console.log('parsedJSON', parsedJSON);
+
     return {
       key: node.id,
       component: (
@@ -20,7 +34,7 @@ export const toGrid = (
           cellIcon={<BlockchainIcon blockchainId={node.blockchainId} />}
           cellStatus={<NodeStatus hasBorder status={node.status} />}
           cellType={
-            nodeTypeList.find((n) => n.id === JSON.parse(node_type).id)?.name
+            nodeTypeList.find((n) => n.id === JSON.parse(node.type).id)?.name
           }
         />
       ),
