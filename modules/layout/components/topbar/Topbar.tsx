3f3eaf547d7a501e28bc502c3585c4349b862aae
frontend-joe <frontendjoe@outlook.com>
2023-01-03 15:14:14 +0000

    fixing reference issue

--- modules/layout/components/topbar/Topbar.tsx
+++ modules/layout/components/topbar/Topbar.tsx
@@ -2,7 +2,6 @@ import { TopbarUser } from './TopbarUser';
 // import {TopbarSearch} from "./TopbarSearch";
 import { TopbarBlockvisor } from './TopbarBlockvisor';
 import { TopbarBurger } from './TopbarBurger';
-import { OrganizationDropdown } from '@modules/organization';
 import { styles } from './Topbar.styles';
 import { useRecoilValue } from 'recoil';
 import { layoutState } from '@modules/layout/store/layoutAtoms';
@@ -14,10 +13,7 @@ export const Topbar = () => {
     <div
       css={[styles.wrapper, layout === 'sidebar' && styles.wrapperSidebarOpen]}
     >
-      <div css={[styles.actionsLeft]}>
-        {/* <TopbarBurger /> */}
-        <OrganizationDropdown hideName hiddenOnDesktop />
-      </div>
+      <div css={[styles.actionsLeft]}>{/* <TopbarBurger /> */}</div>
       <TopbarBlockvisor />
       {/* <TopbarSearch /> */}
       <TopbarUser />
