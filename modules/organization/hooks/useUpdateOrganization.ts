b285b7919e33625e0046e89eb89c5f65926fee69
Dragan Rakita <draganrakita6@gmail.com>
2023-01-10 16:33:03 +0100

    fix: improved org update

--- modules/organization/hooks/useUpdateOrganization.ts
+++ modules/organization/hooks/useUpdateOrganization.ts
@@ -2,22 +2,30 @@ import { Organization } from '@blockjoy/blockjoy-grpc/dist/out/common_pb';
 import { isResponeMetaObject } from '@modules/auth';
 import { ApplicationError } from '@modules/auth/utils/Errors';
 import { apiClient } from '@modules/client';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { organizationAtoms } from '../store/organizationAtoms';
 
 export function useUpdateOrganization() {
+  const [selectedOrganization, setSelectedOrganization] =  useRecoilState(organizationAtoms.selectedOrganization);
   const [loadingState, setLoadingState] = useRecoilState(
     organizationAtoms.organizationLoadingState,
   );
 
-  const updateOrganization = async (id: string, name: string) => {
+  const updateOrganization = async (name: string) => {
     setLoadingState('loading');
-    const organisation = new Organization();
-    organisation.setName(name);
-    organisation.setId(id);
-    const response = await apiClient.updateOrganization(organisation);
+
+    const organization = new Organization();
+    organization.setName(name);
+    organization.setId(selectedOrganization?.id ?? '');
+    const response = await apiClient.updateOrganization(organization);
 
     if (isResponeMetaObject(response)) {
+      const newOrg = {
+        ...selectedOrganization,
+        name,
+      };
+      setSelectedOrganization(newOrg);
+
       setLoadingState('finished');
       return;
     } else {
