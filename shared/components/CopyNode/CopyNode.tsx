ac963b1ac1ba61a16471cced39290a77c45ba249
Bojan Bušljeta <bojan@prototyp.digital>
2023-01-16 11:05:15 +0100

    add fallback copy to clipboard

--- shared/components/CopyNode/CopyNode.tsx
+++ shared/components/CopyNode/CopyNode.tsx
@@ -3,17 +3,36 @@ import { reset } from 'styles/utils.reset.styles';
 import { styles } from './CopyNode.styles';
 import CopyIcon from '@public/assets/icons/copy-12.svg';
 import { colors } from 'styles/utils.colors.styles';
+import { toast } from 'react-toastify';
 
 interface Props {
   disabled?: boolean;
   value: string;
   children: React.ReactNode;
 }
+const isClipboardApiSupported = () => !!(navigator && navigator.clipboard);
 
 export const CopyNode: FC<Props> = ({ disabled, children, value }) => {
+  const fallbackCopyToClipboard = (str: string) => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+
   function handleCopy() {
-    // This should trigger a toast when ready.
-    value && navigator.clipboard.writeText(value);
+    if (isClipboardApiSupported()) {
+      navigator.clipboard.writeText(value);
+      toast.success('Copied');
+    } else {
+      fallbackCopyToClipboard(value);
+      toast.success('Copied');
+    }
   }
 
   return (
