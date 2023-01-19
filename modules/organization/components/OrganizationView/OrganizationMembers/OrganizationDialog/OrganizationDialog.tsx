3553206fec6b8ee616e7138d89528a106f4a9dd9
Dragan Rakita <draganrakita6@gmail.com>
2023-01-19 10:06:01 +0100

    feat: confirmation dialog for all members

--- modules/organization/components/OrganizationView/OrganizationMembers/OrganizationDialog/OrganizationDialog.tsx
+++ modules/organization/components/OrganizationView/OrganizationMembers/OrganizationDialog/OrganizationDialog.tsx
@@ -1,56 +1,79 @@
 import { useInvitations } from '@modules/organization/hooks/useInvitations';
+import { useRemoveMember } from '@modules/organization/hooks/useRemoveMember';
 import { organizationAtoms } from '@modules/organization/store/organizationAtoms';
-import { Member } from '@modules/organization/utils/mapOrganizationMembersToRows';
+import {
+  Action,
+  Member,
+} from '@modules/organization/utils/mapOrganizationMembersToRows';
 import { Button, Modal } from '@shared/components';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { spacing } from 'styles/utils.spacing.styles';
 import { typo } from 'styles/utils.typography.styles';
 import { styles } from './OrganizationDialog.styles';
 
 export type DialogProps = {
   activeMember: Member;
+  activeAction: Action;
   onHide: VoidFunction;
 };
 
 export function OrganizationDialog({
-  activeMember: { invitation_id, email },
+  activeMember: { email, invitation_id, user_id, org_id },
+  activeAction,
   onHide,
 }: DialogProps) {
-  const [sentInvitations, setSentInvitations] = useRecoilState(
-    organizationAtoms.organizationSentInvitations,
-  );
-
   const selectedOrganization = useRecoilValue(
     organizationAtoms.selectedOrganization,
   );
 
   const { revokeInvitation } = useInvitations();
+  const { removeMemberFromOrganization } = useRemoveMember();
 
   const handleRevokeInvitation = async () => {
-    await revokeInvitation({ invitationId: invitation_id, email }, () =>
-      updateInvitations(invitation_id!),
-    );
+    await revokeInvitation({ invitationId: invitation_id, email });
+  };
+
+  const handleRemoveMember = async () => {
+    await removeMemberFromOrganization(user_id!, org_id!);
+  };
+
+  const handleSubmit = () => {
+    switch (activeAction) {
+      case Action.remove:
+        handleRemoveMember();
+        break;
+      case Action.revoke:
+        handleRevokeInvitation();
+        break;
+      default:
+        break;
+    }
+
     onHide();
   };
 
-  const updateInvitations = (invitation_id: string) => {
-    const newSentInvitations = sentInvitations.filter(
-      (invitation) => invitation.id !== invitation_id,
-    );
-    setSentInvitations(newSentInvitations);
+  const messages = {
+    [Action.remove]: {
+      headline: `Remove 1 member from ${selectedOrganization?.name}`,
+      subheadline: 'The following members will be removed:',
+    },
+    [Action.revoke]: {
+      headline: `Canceling 1 invitation from ${selectedOrganization?.name}`,
+      subheadline: 'The following invitations will be canceled:',
+    },
   };
 
   return (
     <Modal portalId="modal-root" isOpen={true} handleClose={onHide}>
       <h2 css={[typo.medium, spacing.bottom.medium]}>
-        Remove 1 member from {selectedOrganization?.name}
+        {messages[activeAction].headline}
       </h2>
       <div css={spacing.bottom.medium}>
-        <p css={spacing.bottom.micro}>The following members will be removed:</p>
+        <p css={spacing.bottom.micro}>{messages[activeAction].subheadline}</p>
         <small>{email}</small>
       </div>
       <div css={[styles.actions, spacing.top.medium]}>
-        <Button onClick={() => handleRevokeInvitation()} style="warning">
+        <Button style="warning" onClick={handleSubmit}>
           Confirm
         </Button>
         <Button style="outline" onClick={onHide}>
