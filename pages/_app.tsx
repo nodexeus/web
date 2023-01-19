a12270b745d3101b479f89009fc614ca473964be
Bojan Bušljeta <bojan@prototyp.digital>
2023-01-19 14:02:08 +0100

    remove package-lock.json, add plausible data domain as env var

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: any) {
       <ThemeProvider>
         <Script
           defer
-          data-domain="app.dev.blockjoy.com"
+          data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN}
           src="https://plausible.io/js/script.js"
         ></Script>
         <Script id="crisp-js">{`window.$crisp=[];window.CRISP_WEBSITE_ID="09ccc057-4bc5-4c14-909a-6b10f6bbe1cb";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}</Script>
