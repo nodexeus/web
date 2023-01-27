059093f0047a3025eba1e4c4d7395d691a23a4cb
Dragan Rakita <draganrakita6@gmail.com>
2023-01-27 16:29:35 +0100

    feat: FAQ via crisp

--- shared/components/Chat/Chat.tsx
+++ shared/components/Chat/Chat.tsx
@@ -1,13 +1,9 @@
-import Script from 'next/script';
+import { Crisp } from 'crisp-sdk-web';
+import { useEffect } from 'react';
 export function Chat() {
-  return (
-    <>
-      <Script
-        defer
-        data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN}
-        src="https://plausible.io/js/script.js"
-      ></Script>
-      <Script id="crisp-js">{`window.$crisp=[];window.CRISP_WEBSITE_ID="09ccc057-4bc5-4c14-909a-6b10f6bbe1cb";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}</Script>
-    </>
-  );
+  useEffect(() => {
+    Crisp.configure(process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID!);
+  }, []);
+
+  return null;
 }
