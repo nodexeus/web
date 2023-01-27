b161c22c1f2daf2e09d57f1e151d7aa59eb5bdae
Dragan Rakita <draganrakita6@gmail.com>
2023-01-27 10:18:03 +0100

    fix: dynamic routes when logged out

--- pages/nodes/[id].tsx
+++ pages/nodes/[id].tsx
@@ -1,10 +1,26 @@
 import { AppLayout } from '@modules/layout';
 import { NodeView } from '@modules/node/';
+import type { GetServerSidePropsContext, PreviewData } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 
 const Node = () => <NodeView />;
 
 Node.getLayout = function getLayout(page: any) {
   return <AppLayout pageTitle="Node">{page}</AppLayout>;
+};
+
+export const getServerSideProps = <
+  Q extends ParsedUrlQuery,
+  D extends PreviewData,
+>(
+  context: GetServerSidePropsContext<Q, D>,
+) => {
+  let { slug } = context.query;
+  if (!slug) {
+    slug = undefined;
+  }
+
+  return { props: { slug: slug || null } };
 };
 
 export default Node;
