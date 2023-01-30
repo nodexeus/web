93d9aa61056972a5638e7ec32be1fff8f8190270
frontend-joe <frontendjoe@outlook.com>
2023-01-30 23:43:22 +0000

    initial commit

--- modules/layout/components/AppLayout.tsx
+++ modules/layout/components/AppLayout.tsx
@@ -13,6 +13,7 @@ import Head from 'next/head';
 import { useGetBlockchains, useNodeList } from '@modules/node';
 import { useRecoilValue } from 'recoil';
 import { useNodeUpdates } from '@modules/node/hooks/useNodeUpdates';
+import { apiClient } from '@modules/client';
 
 type LayoutType = {
   children: React.ReactNode;
@@ -48,6 +49,10 @@ export const AppLayout: React.FC<LayoutType> = ({
     if (!blockchains?.length) getBlockchains();
     getReceivedInvitations(userId!);
     loadNodes();
+  }, []);
+
+  useEffect(() => {
+    apiClient.getUpdates();
   }, []);
 
   useEffect(() => {
