fd4678856c096c67a80994c2661ac59cea509a69
frontend-joe <frontendjoe@outlook.com>
2023-01-30 23:47:40 +0000

    subscribe to upstream

--- modules/layout/components/AppLayout.tsx
+++ modules/layout/components/AppLayout.tsx
@@ -41,7 +41,7 @@ export const AppLayout: React.FC<LayoutType> = ({
   }, []);
 
   useEffect(() => {
-    apiClient.getUpdates();
+    apiClient.getUpdates((data: any) => console.log('client callback', data));
   }, []);
 
   useEffect(() => {
