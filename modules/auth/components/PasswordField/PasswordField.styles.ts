fe4649c6b3dce70a4e7ab59b23099ce37de5168d
Dragan Rakita <draganrakita6@gmail.com>
2023-02-14 11:13:56 +0100

    fix: styling

--- modules/auth/components/PasswordField/PasswordField.styles.ts
+++ modules/auth/components/PasswordField/PasswordField.styles.ts
@@ -1,12 +1,7 @@
 import { css } from '@emotion/react';
-import { breakpoints } from 'styles/variables.styles';
 
 export const styles = {
-  wrapper: (maxWidth: number) => () =>
-    css`
-      position: relative;
-      @media ${breakpoints.toSml} {
-        max-width: ${maxWidth}px;
-      }
-    `,
+  wrapper: css`
+    position: relative;
+  `,
 };
