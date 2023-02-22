3eef35684468d4b69c7a1f0df52dda4a96320c79
Dragan Rakita <draganrakita6@gmail.com>
2023-02-22 18:31:38 +0100

    fix: moved sort and paginate to table component, fixed errors with org members

---
+++ shared/components/Table/utils/sort.ts
@@ -0,0 +1,19 @@
+import { Sorting } from '@modules/organization/ui/OrganizationsUIHelpers';
+
+export const sort = (items: any, sorting: Sorting) => {
+  return [...items].sort((a: ClientOrganization, b: ClientOrganization) => {
+    let aField = a[sorting.field];
+    let bField = b[sorting.field];
+
+    if (typeof aField === 'string') aField = aField.toLowerCase();
+    if (typeof bField === 'string') bField = bField.toLowerCase();
+
+    return sorting.order === 'asc'
+      ? aField > bField
+        ? 1
+        : -1
+      : bField > aField
+      ? 1
+      : -1;
+  });
+};
