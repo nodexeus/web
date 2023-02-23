6e6c32efe68be429a60cbdb2b7a02e8da2ab3993
frontend-joe <frontendjoe@outlook.com>
2023-02-23 10:46:01 +0000

    allowing single quites

--- shared/utils/escapeHtml.ts
+++ shared/utils/escapeHtml.ts
@@ -1,11 +1,11 @@
 import { escape } from 'lodash';
 
-export const escapeHtml = (unsafe: string) => escape(unsafe);
+//export const escapeHtml = (unsafe: string) => escape(unsafe);
 
-// export const escapeHtml = (unsafe: string) =>
-//   unsafe
-//     .replace(/&/g, '&amp;')
-//     .replace(/</g, '&lt;')
-//     .replace(/>/g, '&gt;')
-//     .replace(/"/g, '&quot;')
-//     .replace(/'/g, '&#039;');
+export const escapeHtml = (unsafe: string) =>
+  unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+// .replace(/'/g, '&#039;');
