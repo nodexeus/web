9e4ff3c1edc94b2e3cdfc497bbbee7bcb363084c
Dragan Rakita <draganrakita6@gmail.com>
2023-02-27 19:18:44 +0100

    fix: types

--- shared/components/Table/types/Filter.d.ts
+++ shared/components/Table/types/Filter.d.ts
@@ -4,3 +4,26 @@ type FilteringItem = {
   dataField: string;
   entries: any[];
 };
+
+type Pagination = {
+  currentPage?: number;
+  itemsPerPage?: number;
+};
+
+type Sorting = {
+  order?: 'asc' | 'desc';
+  field?: string;
+};
+
+type Filtering = {
+  [key: string]: {
+    dataField: string;
+    value: string | number;
+  };
+};
+
+type InitialFilter = {
+  pagination?: Pagination;
+  sorting?: Sorting;
+  filtering?: Filtering;
+};
