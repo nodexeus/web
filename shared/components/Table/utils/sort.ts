20c0825b2cc9f00f9fc0869eabaf6dadde5766c7
Dragan Rakita <draganrakita6@gmail.com>
2023-02-27 19:54:41 +0100

    fix: sorting func

--- shared/components/Table/utils/sort.ts
+++ shared/components/Table/utils/sort.ts
@@ -1,10 +1,23 @@
 export const sort = (items: any, sorting: Sorting) => {
   return [...items].sort((a: ClientOrganization, b: ClientOrganization) => {
-    let aField = a[sorting.field!].toLowerCase();
-    let bField = b[sorting.field!].toLowerCase();
+    let aField = a[sorting.field!];
+    let bField = b[sorting.field!];
+
+    if (typeof aField === 'string' && typeof bField === 'string') {
+      aField = aField.toLowerCase();
+      bField = bField.toLowerCase();
+
+      return sorting.order === 'asc'
+        ? aField.localeCompare(bField)
+        : bField.localeCompare(aField);
+    }
 
     return sorting.order === 'asc'
-      ? aField.localeCompare(bField)
-      : bField.localeCompare(aField);
+      ? aField > bField
+        ? 1
+        : -1
+      : bField > aField
+      ? 1
+      : -1;
   });
 };
