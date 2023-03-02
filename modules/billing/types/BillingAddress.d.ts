68042c57973bb3a35269cb9b2067f460c920d521
Dragan Rakita <draganrakita6@gmail.com>
2023-03-02 16:26:54 +0100

    feat: sc-410 invoices ui context

--- modules/billing/types/BillingAddress.d.ts
+++ modules/billing/types/BillingAddress.d.ts
@@ -1,23 +1,23 @@
 interface IBillingAddressHook {
-  loading: boolean;
-  form: UseFormReturn<CreditCardForm>;
-  onSubmit: SubmitHandler<BillingAddressForm>;
-  name: string;
-  handleNameChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  company: string;
-  handleCompanyChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  address: string;
-  handleAddressChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  city: string;
-  handleCityChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  country: string;
-  handleCountryChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  region: string;
-  handleRegionChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  postal: string;
-  handlePostalChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  vat: string;
-  handleVatChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  loading?: boolean;
+  form?: UseFormReturn<CreditCardForm>;
+  onSubmit?: SubmitHandler<BillingAddressForm>;
+  name?: string;
+  handleNameChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  company?: string;
+  handleCompanyChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  address?: string;
+  handleAddressChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  city?: string;
+  handleCityChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  country?: string;
+  handleCountryChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  region?: string;
+  handleRegionChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  postal?: string;
+  handlePostalChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  vat?: string;
+  handleVatChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 type BillingAddressForm = {
--- modules/billing/types/CreditCard.d.ts
+++ modules/billing/types/CreditCard.d.ts
@@ -1,19 +1,19 @@
 interface ICreditCardHook {
-  loading: boolean;
-  form: UseFormReturn<CreditCardForm>;
-  onSubmit: SubmitHandler<CreditCardForm>;
-  cardNumber: string;
-  cardNumberController: any;
-  handleCardNumberChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  cardHolder: string;
-  cardHolderController: any;
-  handleCardHolderChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  expDate: string;
-  expDateController: any;
-  handleExpDateChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  cvc: string;
-  cvcController: any;
-  handleCvcChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  loading?: boolean;
+  form?: UseFormReturn<CreditCardForm>;
+  onSubmit?: SubmitHandler<CreditCardForm>;
+  cardNumber?: string;
+  cardNumberController?: any;
+  handleCardNumberChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  cardHolder?: string;
+  cardHolderController?: any;
+  handleCardHolderChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  expDate?: string;
+  expDateController?: any;
+  handleExpDateChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  cvc?: string;
+  cvcController?: any;
+  handleCvcChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 type CreditCardForm = {
