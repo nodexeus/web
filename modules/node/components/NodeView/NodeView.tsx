85fb9f7aa5e2079b1732faf974938ab68c9696fc
frontend-joe <frontendjoe@outlook.com>
2023-04-13 20:57:52 +0100

    adding node dashboard html

--- modules/node/components/NodeView/NodeView.tsx
+++ modules/node/components/NodeView/NodeView.tsx
@@ -79,7 +79,7 @@ export function NodeView() {
                   date={node?.created!}
                   id={node?.id!}
                 />
-                <NodeViewCharts nodeId={node?.id} />
+                <NodeViewCharts nodeId={node?.id!} />
                 <DetailsTable bodyElements={node?.details!} />
               </>
             ) : (
--- modules/node/components/NodeView/NodeViewCharts.tsx
+++ modules/node/components/NodeView/NodeViewCharts.tsx
@@ -1,20 +1,5 @@
-import { FC, PropsWithChildren, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import { FC } from 'react';
 import { styles } from './NodeViewCharts.styles';
-
-let iframe;
-
-const Iframe: FC<PropsWithChildren> = ({ children }) => {
-  const containerEl = useRef(null);
-
-  useEffect(() => {}, []);
-
-  return (
-    <iframe title="iframe" ref={(el) => (iframe = el)}>
-      {ReactDOM.createPortal(children, document.body)}
-    </iframe>
-  );
-};
 
 type Props = {
   nodeId: string;
@@ -26,7 +11,7 @@ export const NodeViewCharts: FC<Props> = ({ nodeId }) => {
       css={styles.iframe}
       width="100%"
       height="150px"
-      src={`http://127.0.0.1:5555/index.html?node_id=${nodeId}`}
+      src={`/dashboards/node.html?node_id=${nodeId}`}
     />
   );
 };
