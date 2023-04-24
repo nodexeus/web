8b962c5ee5220f11720632988d0eb70e26afbe9d
Dragan Rakita <draganrakita6@gmail.com>
2023-04-24 13:40:07 +0200

    fix: updated env in chargebee instance

--- utils/billing/chargebeeInstance.js
+++ utils/billing/chargebeeInstance.js
@@ -3,8 +3,8 @@ import { ChargeBee } from 'chargebee-typescript';
 let chargebee = new ChargeBee();
 
 chargebee.configure({
-  site: process.env.CHARGEBEE_SITE,
-  api_key: process.env.CHARGEBEE_API_KEY,
+  site: process.env.NEXT_PUBLIC_CHARGEBEE_SITE,
+  api_key: process.env.NEXT_PUBLIC_CHARGEBEE_API_KEY,
 });
 
 export { chargebee };
