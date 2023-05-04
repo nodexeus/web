7e60285b00e50ade41ec3c628c2caa9364d59b90
frontend-joe <frontendjoe@outlook.com>
2023-05-04 11:58:22 +0100

    removing blockjoy npm

--- modules/auth/utils/authTypeGuards.ts
+++ modules/auth/utils/authTypeGuards.ts
@@ -1,19 +1,15 @@
 import { StatusResponse } from '@modules/grpc/status_response';
-import {
-  ApiToken,
-  ResponseMeta,
-} from '@blockjoy/blockjoy-grpc/dist/out/common_pb';
 
-function isSuccess(value: unknown): value is ApiToken.AsObject {
-  return (value as ApiToken.AsObject).value !== undefined;
+function isSuccess(value: unknown): value is any {
+  return (value as any).value !== undefined;
 }
 
 function isLoginError(value: unknown): value is StatusResponse {
   return (value as StatusResponse).code !== undefined;
 }
 
-function isResponeMetaObject(value: unknown): value is ResponseMeta.AsObject {
-  return (value as ResponseMeta.AsObject).status !== undefined;
+function isResponeMetaObject(value: unknown): value is any {
+  return (value as any).status !== undefined;
 }
 
 export { isSuccess, isLoginError, isResponeMetaObject };
