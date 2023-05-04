84b1fbd950c240bd3e8b00bf095336925157c6af
frontend-joe <frontendjoe@outlook.com>
2023-05-04 11:54:48 +0100

    removing blockjoy npm

--- modules/organization/hooks/useCreateOrganization.ts
+++ modules/organization/hooks/useCreateOrganization.ts
@@ -1,13 +1,5 @@
-import { ResponseMeta } from '@blockjoy/blockjoy-grpc/dist/out/common_pb';
 import { organizationClient } from '@modules/grpc';
-import { isResponeMetaObject } from '@modules/auth';
 import { ApplicationError } from '@modules/auth/utils/Errors';
-
-// used for generating mock member count
-
-function isSuccess(response: ResponseMeta.AsObject) {
-  return response.status === ResponseMeta.Status.STATUS_SUCCESS;
-}
 
 export function useCreateOrganization() {
   const createOrganization = async (
