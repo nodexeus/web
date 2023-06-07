7a775967e2cc87a8346878bcb25b715b5a44676e
frontend-joe <frontendjoe@outlook.com>
2023-06-07 12:54:17 +0100

    node dashboard ui tweaks

--- modules/node/utils/mapNodeToDashboardDetails.tsx
+++ modules/node/utils/mapNodeToDashboardDetails.tsx
@@ -1,10 +1,17 @@
 import { Node } from '@modules/grpc/library/blockjoy/v1/node';
+import { ROUTES } from '@shared/constants/routes';
+import Link from 'next/link';
 
 export const mapNodeToDashboardDetails = (node: Node) => {
   if (!node?.nodeType) return [];
 
   const details: { label: string; data: any | undefined }[] = [
-    { label: 'HOST', data: node.hostName || 'Unknown' },
+    {
+      label: 'HOST',
+      data:
+        <Link href={ROUTES.HOST(node.hostId)}>{node.hostName}</Link> ||
+        'Unknown',
+    },
     { label: 'VERSION', data: node.version || 'Latest' },
     { label: 'NODE ADDRESS', data: node.address || '-' },
   ];
