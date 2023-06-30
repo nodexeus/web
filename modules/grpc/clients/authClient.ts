162d58400564d833add9bf61b44d286d3f3766f5
frontend-joe <frontendjoe@outlook.com>
2023-06-30 16:41:30 +0100

    using useSignIn hook

--- modules/grpc/clients/authClient.ts
+++ modules/grpc/clients/authClient.ts
@@ -58,7 +58,6 @@ class AuthClient {
     try {
       const response = await this.client.confirm({}, authHeader);
       console.log('token from confirmation', response);
-      setTokenValue(response.token);
       return response.token;
     } catch (err) {
       return handleError(err);
