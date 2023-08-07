703f50dd603c351033bfc2bed155ca21845044a4
StjepanDzojic <stjepan@prototyp.digital>
2023-08-07 14:24:43 +0200

    Added hyperlink

--- shared/components/General/Copy/Copy.tsx
+++ shared/components/General/Copy/Copy.tsx
@@ -1,6 +1,7 @@
 import CopyIcon from '@public/assets/icons/common/Copy.svg';
 import { copyToClipboard } from '@shared/utils/copyToClipboard';
 import { Button, SvgIcon } from '@shared/components';
+import { spacing } from 'styles/utils.spacing.styles';
 
 interface CopyButtonProps {
   disabled?: boolean;
@@ -16,6 +17,7 @@ export const Copy = ({ disabled, value }: CopyButtonProps) => {
       size="small"
       onClick={handleCopy}
       disabled={disabled}
+      css={spacing.left.small}
     >
       Copy <CopyIcon />
     </Button>
