b35916c29d6f3fc57b07a55ec95d70d2f59e620c
frontend-joe <frontendjoe@outlook.com>
2023-08-09 01:20:52 +0100

    sc-2165 - hide rpc requests on host

--- modules/host/components/HostView/HostViewSidePanel/HostViewSidePanel.tsx
+++ modules/host/components/HostView/HostViewSidePanel/HostViewSidePanel.tsx
@@ -3,5 +3,7 @@ import { NetdataDashboard } from '@shared/components';
 
 export const HostViewSidePanel = () => {
   const { host } = useHostView();
-  return <NetdataDashboard id={host?.name!} />;
+  return (
+    <NetdataDashboard id={host?.name!} disk_space_name="_var_lib_blockvisor" />
+  );
 };
