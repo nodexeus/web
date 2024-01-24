9e89dac518b886911f142c7080122c5ec6114adb
frontend-joe <frontendjoe@outlook.com>
2024-01-24 21:34:39 +0000

    ui tweaks part fifty-noine

--- modules/commands/components/CommandsView.styles.ts
+++ modules/commands/components/CommandsView.styles.ts
@@ -3,7 +3,7 @@ import { breakpoints } from 'styles/variables.styles';
 
 export const styles = {
   wrapper: css`
-    background-color: #1f2120;
+    background-color: #1e201f;
     border-radius: 6px;
     padding: 20px;
 
