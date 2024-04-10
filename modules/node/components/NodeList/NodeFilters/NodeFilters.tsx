855307624ae95535b5c25eee297c0a279f02bfec
Dragan Rakita <draganrakita6@gmail.com>
2024-04-10 18:30:55 +0200

    chore: build fix

--- modules/node/components/NodeList/NodeFilters/NodeFilters.tsx
+++ modules/node/components/NodeList/NodeFilters/NodeFilters.tsx
@@ -2,7 +2,7 @@ import { FormEvent, useMemo, useRef, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import IconClose from '@public/assets/icons/common/Close.svg';
 import IconRefresh from '@public/assets/icons/common/Refresh.svg';
-import { styles } from './nodeFilters2.styles';
+import { styles } from './nodeFilters.styles';
 import {
   Skeleton,
   SkeletonGrid,
