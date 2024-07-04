ac8327439faf2e538678cfa180e03bd7d0137b12
frontend-joe <frontendjoe@outlook.com>
2024-07-04 11:20:58 +0100

    adding version description to blockchain admin and version select

--- modules/node/components/Shared/NodeVersionSelect/NodeVersionSelect.tsx
+++ modules/node/components/Shared/NodeVersionSelect/NodeVersionSelect.tsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { css } from '@emotion/react';
 import { BlockchainVersion } from '@modules/grpc/library/blockjoy/v1/blockchain';
 import { Dropdown } from '@shared/components';
 import { nodeLauncherAtoms } from '@modules/node';
 import { authSelectors } from '@modules/auth';
+import { ITheme } from 'types/theme';
 
 type NodeVersionSelectProps = {
   versions: BlockchainVersion[];
   onVersionChanged: (version: BlockchainVersion | null) => void;
+};
+
+const styles = {
+  versionDescription: (theme: ITheme) => css`
+    color: rgb(255 255 255 / 56%);
+  `,
+  buttonText: (theme: ITheme) => css`
+    color: ${theme.colorText};
+  `,
 };
 
 export const NodeVersionSelect = ({
@@ -27,6 +38,17 @@ export const NodeVersionSelect = ({
       disabled={!isSuperUser || versions.length < 2}
       items={versions}
       itemKey="version"
+      renderButtonText={
+        <p css={styles.buttonText}> {selectedVersion?.version}</p>
+      }
+      renderItem={(item) => (
+        <>
+          {item.version}
+          {isSuperUser && (
+            <span css={styles.versionDescription}> {item.description}</span>
+          )}
+        </>
+      )}
       isOpen={isOpen}
       handleOpen={handleOpen}
       handleSelected={onVersionChanged}
