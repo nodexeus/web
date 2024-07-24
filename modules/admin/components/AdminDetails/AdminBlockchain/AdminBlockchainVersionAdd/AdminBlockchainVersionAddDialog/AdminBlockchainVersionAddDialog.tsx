210692486791eace08657f024e57c911e824426e
frontend-joe <frontendjoe@outlook.com>
2024-07-24 16:09:18 +0100

    enabled admin blockchain version node type dropdown and add new netdata url

--- modules/admin/components/AdminDetails/AdminBlockchain/AdminBlockchainVersionAdd/AdminBlockchainVersionAddDialog/AdminBlockchainVersionAddDialog.tsx
+++ modules/admin/components/AdminDetails/AdminBlockchain/AdminBlockchainVersionAdd/AdminBlockchainVersionAddDialog/AdminBlockchainVersionAddDialog.tsx
@@ -161,7 +161,6 @@ export const AdminBlockchainVersionAddDialog = ({
             />
             <FormLabel>Node type</FormLabel>
             <NodeTypeSelectDropdown
-              disabled
               items={nodeTypes}
               selectedItem={
                 nodeTypes.find(
