10b2df428502a9cdc8c3ba44a37615e4c496815c
frontend-joe <frontendjoe@outlook.com>
2024-12-12 21:27:41 +0000

    host delete and update host display name

--- modules/admin/components/AdminLists/AdminNodes/AdminNodes.tsx
+++ modules/admin/components/AdminLists/AdminNodes/AdminNodes.tsx
@@ -82,16 +82,6 @@ const columns: AdminListColumn[] = [
     // filterComponent: AdminNodesFilterProtocol,
     // filterDropdownMinWidth: 200,
   },
-  // {
-  //   name: 'nodeType',
-  //   displayName: 'Node Type',
-  //   width: '140px',
-  //   sortField: NodeSortField.NODE_SORT_FIELD_NODE_TYPE,
-  //   isVisible: true,
-  //   filterComponent: AdminNodesFilterNodeType,
-  //   filterDropdownMinWidth: 100,
-  //   filterDropdownMaxWidth: 120,
-  // },
   {
     name: 'variant',
     width: '140px',
