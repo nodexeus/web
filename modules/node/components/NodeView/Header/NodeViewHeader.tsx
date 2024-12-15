ab43e162d5819a28330dc9a3e9ab89633128de63
frontend-joe <frontendjoe@outlook.com>
2024-12-15 23:09:54 +0000

    sorting node image properties and enum controls

--- modules/node/components/NodeView/Header/NodeViewHeader.tsx
+++ modules/node/components/NodeView/Header/NodeViewHeader.tsx
@@ -159,7 +159,7 @@ export const NodeViewHeader = () => {
                 <div css={styles.name}>
                   <div css={styles.title}>
                     <EditableTitle
-                      initialValue={node.displayName! || node.nodeName}
+                      initialValue={node.displayName}
                       isLoading={isLoading}
                       isSaving={isSaving!}
                       additionalContentRight={
